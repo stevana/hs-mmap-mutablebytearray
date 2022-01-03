33e3fc2cde8cf073c6f45d58d83bc9c6f66f3018
Stevan Andjelkovic <stevana@users.noreply.github.com>
2022-01-03 14:16:25 +0100

    feat: mmap already mmapped memory seems to work

--- c-mmap/mmap.c
+++ c-mmap/mmap.c
@@ -35,6 +35,42 @@ int step(void) {
   close(fd);
 }
 
+int mmap_mmapped (void) {
+  const int pagesize = getpagesize();
+  char *addr = mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+  if (addr == MAP_FAILED) {
+    printf("mmap 1 failed\n");
+    return(1);
+  }
+
+  int fd = open("/tmp/mmap.txt", O_RDWR);
+  if (fd == -1) {
+    printf("open failed\n");
+    return(1);
+  } else {
+    // printf("opened file, fd: %d\n", fd);
+  }
+  char *addr2 = mmap(addr, pagesize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0);
+  close(fd);
+  if (addr2 == MAP_FAILED) {
+    printf("mmap 2 failed\n");
+    return(2);
+  }
+  int res;
+  res = munmap(addr, 64);
+  if (res == -1) {
+    printf("munmap 1 failed\n");
+    return (1);
+  }
+  munmap(addr2, pagesize);
+  if (res == -1) {
+    printf("munmap 2 failed\n");
+    return (2);
+  }
+
+  return(0);
+}
+
 int main (void) {
 
   int res;
@@ -44,9 +80,10 @@ int main (void) {
   for (i = 0; i < 1000000; i++) {
     if (i % 1000 == 0)
       printf("iteration: %d\n", i);
-    res = step();
+    res = mmap_mmapped();
     if (res != 0) {
       printf("step failed: %d\n", res);
+      exit(res);
     }
   }
 
